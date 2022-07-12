c2acd6dce20300b2c1aedfb13ae77b0a5f837367
Alexandr <sashkavoytov20@gmail.com>
2022-07-12 21:38:21 +0500

    Выолнил ДЗ

---
+++ task0/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+Console.Clear();
+
+Console.Write("Введите первое число ");
+int one = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int two = Convert.ToInt32(Console.ReadLine());
+
+if (one > two)
+{
+     Console.WriteLine($"Число {one}, больше {two}");
+}
+else {
+    Console.WriteLine($"Число {two}, больше {one}");
+}
---
+++ task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Clear();
+
+Console.Write("Введите первое число ");
+int one = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int two = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число ");
+int three = Convert.ToInt32(Console.ReadLine());
+
+int max = one;
+
+if (two > max)
+{
+     max = two;
+}
+if (three > max)
+{
+    max = three;
+}
+
+Console.WriteLine($"Максимальное число {max}");
